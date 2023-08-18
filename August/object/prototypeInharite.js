6704145113061d7aea661d68016d93a9350cbf66
Ahmed-Munna <ahmedmunna4200@gmail.com>
2023-08-18 17:12:15 +0600

    js

---
+++ August/object/prototypeInharite.js
@@ -0,0 +1,20 @@
+
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+function Crickter(type, run) {
+    Person.call(this);
+    this.type = type;
+    this.run = run;
+}
+
+const Sakib = new Crickter('Sakib', 35, 'All-rounder', 5939);
+
+console.log(Sakib.name);
+
+
+
+
+
---
+++ August/object/this.js
@@ -0,0 +1,27 @@
+
+// function Player(obj) {
+//     obj.printPlayerName = function() {
+//         return this.name;
+//     };
+// }
+
+// const Nam = {
+//     name: 'Sakib',
+//     age: 34,
+// }
+// Player(Nam);
+
+// console.log(Nam.printPlayerName())
+
+const Player = {
+    name: 'Sakib',
+    playerName: this.name
+}
+
+console.log(Player)
+
+
+
+
+
+
