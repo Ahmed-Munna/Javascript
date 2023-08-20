ebae4c75624c8750f19a584b93927a7da9a80241
Ahmed-Munna <ahmedmunna4200@gmail.com>
2023-08-20 19:26:15 +0600

    js

---
+++ August/function/paramitar.js
@@ -0,0 +1,32 @@
+
+
+// let a = [2,3,4,5];
+
+// function numb(n) {
+//     n[2] = 40
+
+//     return n;
+// }
+
+// console.log(numb(a))
+// console.log(a)
+
+
+
+// function clickMe(id) {
+//     console.log(id);
+// }
+
+
+let a = [3,4,6,7];
+
+let b = Math.max.apply(null,a);
+
+console.log(b);
+
+
+
+
+
+
+
