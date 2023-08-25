3134887e1dad55ba50edcb936491d2b103f227aa
Ahmed-Munna <ahmedmunna4200@gmail.com>
2023-08-25 22:34:29 +0600

    js

--- August/dom/document/app.js
+++ August/dom/document/app.js
@@ -1,4 +1,14 @@
-const ele = document.getElementById("demo");
-ele.innerHTML = 'Hello';
-console.dir(document)
+const formElement = document.forms["myForm"];
+const alertId = document.getElementById('alert');
+function validForm() {
+    let pattern = /^[a-zA-Z]+ [a-zA-Z]+$/;
+    let fval = formElement['text'].value;
+    if (!pattern.test(fval)) {
+        alertId.style = "display: block; color: red";
+        formElement['text'].style = "border: 1px solid red";
+        console.log(alertId)
+        return false;
+    }
+}
+
 
