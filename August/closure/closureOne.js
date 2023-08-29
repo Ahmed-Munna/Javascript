a29689f43e7e97a7950417fe0872b2e4021ecf52
Ahmed-Munna <ahmedmunna4200@gmail.com>
2023-08-29 19:14:27 +0600

    js

---
+++ August/closure/closureOne.js
@@ -0,0 +1,37 @@
+
+
+// function sum(num1) {
+
+//     return function (num2) {
+//         return num1 + num2;
+//     }
+// }
+
+// var myFunc = sum(4);
+
+// console.dir(myFunc(2));
+// console.dir(myFunc)
+
+for(var i = 1; i < 4; i++) {
+
+    setTimeout(() => {
+        console.log(i)
+    },1000);
+}
+
+// let a ;
+
+// function myFunc() {
+//     a = 32;
+
+//     var fun = function () {
+//         console.log(a);
+//     }
+//     return setTimeout(fun,2000);
+// }
+
+// myFunc();
+// a = 49;
+
+// console.dir(myFunc)
+
