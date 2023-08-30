640fb1417cc8e3ddb0f56268c4795affc3bf294e
Ahmed-Munna <ahmedmunna4200@gmail.com>
2023-08-31 03:42:28 +0600

    js

--- August/prototype/proto.js
+++ August/prototype/proto.js
@@ -1,9 +1,36 @@
+// let a = function() {
+//     let b = "not"
+// }
+
+// a.BD = "bangladesh";
+
+// console.dir(a);
+
+
+// let a = {
+//     name: "munna",
+//     age: 23
+// }
+
+// let b = {
+//     fname: "SHA",
+//     age: 22
+// }
+
+// Object.setPrototypeOf(b,a);
+
+
+// console.log(b.name)
 
 
 function Person(name, age) {
     this.name = name;
     this.age = age;    
 }
+
+Person.prototype.eat = function() {
+    return this.name + " is Eating";
+} 
 
 function Crickter(name, age, type, country) {
     
@@ -13,9 +40,12 @@ function Crickter(name, age, type, country) {
     this.type = type;
     this.country = country;
 }
+Crickter.prototype.eat = Object.create(Person.prototype.eat);
+// Crickter.prototype.constructor = Crickter;
+let Cri = new Crickter("Munna", 23, "all-rounder", "BD");
+
+let per = new Person("Ahmed", 33);
+console.log(Cri.eat);
 
 
-let myFun = new Crickter("Munna", 23, "all-rounder", "BD");
-
-console.log(myFun.name)
 
