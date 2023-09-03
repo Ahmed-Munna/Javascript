031408bc6911fa1b898ece0ed0a41dc5e1b20986
Ahmed-Munna <ahmedmunna4200@gmail.com>
2023-09-03 19:52:58 +0600

    js

---
+++ August/short project/countdown/app.js
@@ -0,0 +1,20 @@
+
+const setTime = document.getElementById("setTime"),
+      pauseTime = document.getElementById("pauseTime"),
+      resetTime = document.getElementById("resetTime"),
+      popup = document.getElementById("popup");
+
+setTime.onclick = () => {
+    popup.style.visibility = "visible";
+    console.log(popup);
+}
+
+
+
+
+
+
+
+
+
+
