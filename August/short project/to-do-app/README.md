6116b0eada06fcfe6538988b19ac592167ca8e42
Ahmed-Munna <ahmedmunna4200@gmail.com>
2023-09-09 03:15:25 +0600

    js

---
+++ August/short project/to-do-app/README.md
@@ -0,0 +1,8 @@
+<h1 align="center">Todo App</h1>
+
+<h3>About</h3>
+<p>The Todo-App is a task managing Web-Application to help its users to stay organized for their day to day activities.</p> 
+<h3>Technologies Used</h3>
+<p>
+<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> </a> 
+<a href="https://www.w3.org/html/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/> </a><a href="https://www.w3schools.com/css/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40"/> </a></p
--- August/short project/to-do-app/app.js
+++ August/short project/to-do-app/app.js
@@ -1,13 +1,97 @@
 const todoInput = document.getElementById("todoInput"),
       addBtn = document.getElementById("addBtn"),
-      notifiy = document.getElementById("notifiy");
+      notifiy = document.getElementById("notifiy"),
+      taskList = document.getElementById("task-list"),
+      updateBtn = document.getElementById("update-btn");
+
+window.onload = () => {
+    let list = localStorage.todoList;
+    if(list) {
+        taskList.innerHTML = list;
+    }
+}
+
+
 
 addBtn.onclick = () => {
     if (
         todoInput.value.charAt(0) === " " || todoInput.value === ""
        ) {  
             notifiy.firstElementChild.innerHTML = "Please add valid input !";
+            notifiy.style.backgroundColor = "red";
             notifiy.style.visibility = "visible";
+            setTimeout(() => {
+                notifiy.style.visibility = "hidden";
+            }, 2000);
+        } else {
+
+            let individulTask = `<li class="li">
+            <span class="todo-text">${todoInput.value}</span>
+            <span class="span-button" onclick="trash(this)"><i class="fa-solid fa-trash"></i></span>
+            <span class="span-button" onclick="update(this)"><i class="fa-solid fa-pen"></i></span>
+            </li>`;
+            
+            taskList.innerHTML += individulTask;
+            store();
+            todoInput.value = "";
+            
+            notifiy.firstElementChild.innerHTML = "Item is successfully added";
+            notifiy.style.visibility = "visible";
+            notifiy.style.backgroundColor = "green";
+            setTimeout(() => {
+                notifiy.style.visibility = "hidden";
+            }, 2000);
+        }
+}
+
+function store() {
+    localStorage.todoList = taskList.innerHTML;
+}
+
+function trash(self) {
+    self.parentNode.remove();
+    store();
+
+    notifiy.firstElementChild.innerHTML = "Deleted Task";
+    notifiy.style.backgroundColor = "red";
+    notifiy.style.visibility = "visible";
+    setTimeout(() => {
+        notifiy.style.visibility = "hidden";
+    }, 2000);
+}
+
+function update(self) {
+    addBtn.style.display = 'none';
+    updateBtn.style.display = 'block';
+    todoInput.value = self.previousSibling.previousSibling.previousElementSibling.innerText;
+    self.parentNode.remove();
+}
+
+updateBtn.onclick = () => {
+    if (
+        todoInput.value.charAt(0) === " " || todoInput.value === ""
+       ) {  
+            notifiy.firstElementChild.innerHTML = "Please add valid input !";
+            notifiy.style.backgroundColor = "red";
+            notifiy.style.visibility = "visible";
+            setTimeout(() => {
+                notifiy.style.visibility = "hidden";
+            }, 2000);
+        } else {
+
+            let individulTask = `<li class="li">
+            <span class="todo-text">${todoInput.value}</span>
+            <span class="span-button" onclick="trash(this)"><i class="fa-solid fa-trash"></i></span>
+            <span class="span-button" onclick="update(this)"><i class="fa-solid fa-pen"></i></span>
+            </li>`;
+            
+            taskList.innerHTML += individulTask;
+            store();
+            todoInput.value = "";
+            
+            notifiy.firstElementChild.innerHTML = "Item is successfully Updated";
+            notifiy.style.visibility = "visible";
+            notifiy.style.backgroundColor = "green";
             setTimeout(() => {
                 notifiy.style.visibility = "hidden";
             }, 2000);
